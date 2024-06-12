f648f671373da2eb6c0d994847f3e4594759b967
David Rivero <david.davidsociety.rivero@gmail.com>
2024-06-12 22:34:54 +0100

    First commit

---
+++ src/main/java/com/example/quermi/QuermiApplication.java
@@ -0,0 +1,13 @@
+package com.example.quermi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuermiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuermiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quermi/QuermiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quermi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuermiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
